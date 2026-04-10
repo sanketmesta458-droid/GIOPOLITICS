8db00311dc4f18c19e2f12ed6d66f57ef5e994e7
sanketmesta31 <sanketmesta458@gmail.com>
2026-04-10 10:23:48 +0530

    Set API base URL before rendering App component

--- geopolitics-dashboard22/frontend/src/main.tsx
+++ geopolitics-dashboard22/frontend/src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setBaseUrl } from "@/api/custom-fetch";
+
+const apiBase = import.meta.env.VITE_API_BASE_URL as string | undefined;
+if (apiBase) {
+  setBaseUrl(apiBase);
+}
 
 createRoot(document.getElementById("root")!).render(<App />);
